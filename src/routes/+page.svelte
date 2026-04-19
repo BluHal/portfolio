c3eb95453dae0a7a6aa8afe885f483ace8e80718
Nicola Redolfi <nredolfi@icloud.com>
2026-04-19 13:08:12 +0200

    refactor: migrate page to Svelte 5; replace onMount wallpaper hack with reactive style

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,13 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
+	import { themeState, getWallpaperStyle } from '$lib/theme.svelte.js';
 	import Taskbar from '../components/Taskbar.svelte';
 	import Desktop from '../components/Desktop.svelte';
-	let selectedWallpaper = 'monocolor-wallpaper.png';
-
-	onMount(() => {
-		document.documentElement.style.backgroundImage = `url('/images/${selectedWallpaper}')`;
-		document.documentElement.style.backgroundSize = `cover`;
-		document.documentElement.style.backgroundRepeat = `no-repeat`;
-	});
 </script>
 
-<Desktop />
-<Taskbar />
+<div
+	class="w-dvw h-dvh overflow-hidden"
+	style={getWallpaperStyle(themeState.wallpaper, themeState.wallpaperUrl)}
+>
+	<Desktop />
+	<Taskbar />
+</div>
